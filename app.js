d5a54cb6c8ddd0ee9fb01b0b3414b32593916a22
sunny-github-acc <54981869+sunny-github-acc@users.noreply.github.com>
2022-11-14 16:28:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const serverless = require('serverless-http');
-
-const router = require('./src/routes');
+const router = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +10,6 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
-app.use('/', router);
+app.use('/.netlify/functions/app', router);
 
 module.exports = app;
-module.exports.handler = serverless(app);
