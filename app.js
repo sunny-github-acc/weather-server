e20134fa740cc40e9f6654a81f02d283b61b7731
sunny-github-acc <54981869+sunny-github-acc@users.noreply.github.com>
2022-11-14 16:18:32 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const router = require('./src/routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, async (error) => console.log(error ? error : ':)'));
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+app.use('/', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
